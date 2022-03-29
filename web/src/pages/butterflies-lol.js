0c9c818aa125a6b5415a4575dced1e51dbe43fa8
Drew Hill <mdude2314@gmail.com>
2022-03-29 19:59:02 -0400

    Add static butterflies

--- web/src/pages/butterflies-lol.js
+++ web/src/pages/butterflies-lol.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import '../lib/jsmpeg.min.js';
+// import '../lib/jsmpeg.min.js';
 
 const rawScript = `player = new JSMpeg.Player("ws://75.53.186.22:5565", {
   canvas: document.getElementById("canvas"), // Canvas should be a canvas DOM element
 });`
 
 const ButterfliesPage = () => (
-  <html>
-    <body>
-      <canvas id="canvas"></canvas>
-    </body>
-    {/* <script src={"jsmpeg.min.js"}></script> */}
+  <>
+    <canvas id="canvas"></canvas>
+
+    <script src={"https://cdnjs.cloudflare.com/ajax/libs/jsmpeg/0.2/jsmpg.min.js"}></script>
     <script dangerouslySetInnerHTML={{ __html: rawScript }}></script>
-  </html>
+  </>
 
 );
 
