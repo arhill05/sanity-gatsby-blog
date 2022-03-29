a12edc3fcdeab5b608a91b68b1dba5fed877783d
Drew Hill <mdude2314@gmail.com>
2022-03-29 19:49:36 -0400

    IMPORT

--- web/src/pages/butterflies.js
+++ web/src/pages/butterflies.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../lib/jsmpeg.min.js';
 
 const rawScript = `player = new JSMpeg.Player("ws://75.53.186.22:5565", {
   canvas: document.getElementById("canvas"), // Canvas should be a canvas DOM element
@@ -9,7 +10,7 @@ const ButterfliesPage = () => (
     <body>
       <canvas id="canvas"></canvas>
     </body>
-    <script src={"jsmpeg.min.js"}></script>
+    {/* <script src={"jsmpeg.min.js"}></script> */}
     <script dangerouslySetInnerHTML={{ __html: rawScript }}></script>
   </html>
 
