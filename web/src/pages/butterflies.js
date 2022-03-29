e6749ad2d3486a9e45415a7651b991f0b81f4eb9
Drew Hill <mdude2314@gmail.com>
2022-03-29 18:29:05 -0400

    fix butterflies

--- web/src/pages/butterflies.js
+++ web/src/pages/butterflies.js
@@ -1,5 +1,5 @@
 const ButterfliesPage = () => (
-  <html>
+  <>
     <body>
       <canvas id="canvas"></canvas>
     </body>
@@ -12,8 +12,7 @@ const ButterfliesPage = () => (
         })
       }
     </script>
-  </html>
-
+  </>
 );
 
 export default ButterfliesPage;
